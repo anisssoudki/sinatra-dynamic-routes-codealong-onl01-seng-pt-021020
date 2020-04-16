276c0b617f89dbf4b1f4ad9eff0692f492565aa2
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-16 10:25:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,13 +4,14 @@ class App < Sinatra::Base
 
   # This is a sample static route.
   get '/hello' do
-    "Hello World!"
+    erb :index
   end
 
   # This is a sample dynamic route.
   get "/hello/:name" do
     @user_name = params[:name]
     "Hello #{@user_name}!"
+    
   end
 
   # Code your final two routes here:
